94938c716ee7e3c037a484e88968137d2a38329c
Fernando Antunes <f.antunes84@gmail.com>
2023-06-14 12:11:58 -0300

    FIX: Reconfiguração do menu

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from game_functions import general_functions, handle_log
 from game_functions import get_screen
 
 
+def print_menu():
+    print('Opções válidas:')
+    print('darkforces - Ataca a força das trevas. (É necessário passar a VIT atual).')
+    print('boss       - Ataca o chefão mundial.')
+    print('refugee    - Resgate de refugiados. (É necessário passar a VIT atual).')
+    print('gem        - Coleta os diamantes diários gratuitos. (É necessário passar o número de clicks).')
+    print('cursor     - Printa no console a posição do cursor')
+
 
 try:
     param1 = sys.argv[1]
@@ -23,6 +31,10 @@ try:
         param2 = sys.argv[2]
         vit = int(param2)
         general_functions.rescue_refugees(vit)
+    if param1 == 'freegem':
+        param2 = sys.argv[2]
+        clicks = int(param2)
+        general_functions.free_gem(clicks)
     if param1 == 'cursor':
         general_functions.get_cursor_position()
     # else:
@@ -30,16 +42,10 @@ try:
 except NameError as message:
     if 'param1' == message.name:
         print('*** ERRO *** O parâmetro 1 é obrigatório.\n')
-        print('Opções válidas:')
-        print('darkforces - Ataca a força das trevas. (É necessário passar a vit atual).')
-        print('getcursor  - Printa no console a posição do cursor')
-        print('level      - Obtém o nível da força das trevas a ser atacada.')
+        print_menu()
+
 except KeyError as message:
     print('*** ERRO *** Opção de parâmetro inválida.\n')
-    print('Opções válidas:')
-    print('darkforces - Ataca a força das trevas. (É necessário passar a vit atual).')
-    print('getcursor  - Printa no console a posição do cursor')
-    print('level      - Obtém o nível da força das trevas a ser atacada.')
+    print_menu()
 except KeyboardInterrupt:
     print('*** ENCERRANDO! ***')
-
